fe6ac4b1a2ad0c9b1d348ced95a137aef9d869af
drewLM <dphilippidis2@gmail.com>
2024-09-16 19:56:15 +0100

    Create CropperTool.js

---
+++ CropperTool.js
@@ -0,0 +1,23 @@
+
+window.addEventListener('DOMContentLoaded', function () {
+  var image = document.querySelector('#image');
+  var cropper = new Cropper(image, {
+    viewMode: 1,
+    dragMode: 'move',
+    aspectRatio: 2100/100,
+    autoCropArea: 1,
+    restore: false,
+    center: false,
+    cropBoxMovable: false,
+    cropBoxResizable: false,
+    toggleDragModeOnDblclick: false,
+    zoomable: false,
+    background:false,
+    guides: false,
+    center: false,
+
+    
+  });
+
+
+});
